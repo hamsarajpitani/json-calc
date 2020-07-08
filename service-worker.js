ea90206ee12c9cf8a2289405419793dfed7871dc
PitaniHamsaraj <33552374+hamsarajpitani@users.noreply.github.com>
2020-07-08 11:51:07 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+
+window.addEventListener('beforeinstallprompt', e => {
+    console.log('beforeinstallprompt Event fired');
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+    return false;
+    });
+  // When you want to trigger prompt:
+  this.deferredPrompt.prompt();
+    this.deferredPrompt.userChoice.then(choice => {
+    console.log(choice);
+    });
+  this.deferredPrompt = null;
+// service-worker.js
+self.addEventListener('install', function() {
+    console.log('Install!');
+  });
+  self.addEventListener("activate", event => {
+    console.log('Activate!');
+  });
+  self.addEventListener('fetch', function(event) {
+    console.log('Fetch!', event.request);
+  });
+
+  navigator.serviceWorker.ready.then(function(registration) {
+    if (!registration.pushManager) {
+      alert('No push notifications support.');
+      return false;
+    }
+    //To subscribe `push notification` from push manager
+    registration.pushManager.subscribe({
+    userVisibleOnly: true //Always show notification when received
+    })
+    .then(function (subscription) {
+    console.log('Subscribed.');
+    })
+    .catch(function (error) {
+    console.log('Subscription error: ', error);
+    });
+  })
